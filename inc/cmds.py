35544d2d383cb629b3ceea9745caa43eb29f7b68
Pidu18 <38082683+Pidu18@users.noreply.github.com>
2018-04-23 20:54:41 +0300

    Update cmds.py

--- inc/cmds.py
+++ inc/cmds.py
@@ -3,14 +3,14 @@ from discord.ext import commands
 import random
 import async
 
-class Cmds:
- def _init_(self):
-  self.bot=bot
+class CMDS():
+    def __init__(self, bot):
+    self.bot = bot
   
- @commands.command(pass_context=True)
- async def ping(self):
-  await self.bot.say("Pong")
+    @commands.command(pass_context=True)
+    async def ping(self):
+        await self.bot.say("Pong")
   
 def setup(bot):
- bot.add_cog(Cmds(bot))
+    bot.add_cog(CMDS(bot))
   
