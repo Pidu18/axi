599574cad06ec49ba4dbed7235a71c3c175a51af
Pidu18 <38082683+Pidu18@users.noreply.github.com>
2018-04-23 21:06:59 +0300

    Update cmds.py

--- inc/cmds.py
+++ inc/cmds.py
@@ -10,7 +10,15 @@ class CMDS():
     @commands.command(pass_context=True)
     async def ping(self):
         await self.bot.say("Pong")
+
+    @commands.command(pass_context=True)
+    async def gay(self, ctx, user: discord.Member = None):
+    	gay = random.randint(1, 100)
+    	if user is None:
+    		await self.bot.say("{}, you are {}'%' gay".format(ctx.message.author.mention, gay))
+    	else:
+    		await self.bot.say("{} is {}'%' gay".format(user.mention, gay))
+
   
 def setup(bot):
-    bot.add_cog(CMDS(bot))
-  
+bot.add_cog(CMDS(bot))
